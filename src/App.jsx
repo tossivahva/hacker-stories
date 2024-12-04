d25ceba490ae53e9bfee86a7a71e07f260dfeea8
tossivahva <tossivahva@gmail.com>
2024-12-04 16:38:08 +0300

    search with local stodage + custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import './App.css';
 import * as React from 'react';
+import { useEffect } from 'react';
 
 
 const title = 'React';
+
+// const profile = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+// };
+//
+// const adress = {
+//     country: 'United States',
+//     city: 'California',
+// };
+//
+// const user = {
+//     ...profile,
+//     ...adress,
+//     dog: 'Bob',
+// };
+//
+// console.log(user);
+
+const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+    
+    useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [key, value]);
+    
+    return [value, setValue];
+};
 
 
 function App() {
@@ -24,19 +53,27 @@ function App() {
             objectID: 1,
         },
     ];
-    const [searchTerm, setSearchTerm] = React.useState('');
+    
+    // const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React search');
+    
+    const [searchTerm, setSearchTerm] = useStorageState('search', 'Search here...');
+    
+    // React.useEffect(() => {
+    //     localStorage.setItem('search', searchTerm);
+    // }, [searchTerm]);
     
     const handleSearch = (e) => {
         setSearchTerm(e.target.value);
-        
         console.log(e.target.value);
     };
     
     const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    
     return (
         <div>
             <h1>Hello {title} + Vite!</h1>
-            <Search onSearch={handleSearch}/>
+            <Search search={searchTerm}
+                    onSearch={handleSearch}/>
             <hr/>
             <List list={searchedStories}/>
         </div>
@@ -44,28 +81,28 @@ function App() {
 }
 
 
-const Search = (props) => {
+const Search = ({ search, onSearch }) => {
     
     return (
         <div>
             <label htmlFor="search">{'Search: '}</label>
             <input id="search"
                    type="text"
-                   value={props.onSearch.value}
-                   onChange={props.onSearch}
+                   value={search}
+                   onChange={onSearch}
             />
             <p>
-                Searching for: <strong>{props.onSearch.value}</strong>
+                Searching for: <strong>{search}</strong>
             </p>
         </div>
     );
 };
 
 
-const List = (props) => {
+const List = ({ list }) => {
     return (
         <ul>
-            {props.list.map(item => (
+            {list.map((item) => (
                 <Item key={item.objectID}
                       item={item}/>
             ))}
@@ -73,14 +110,13 @@ const List = (props) => {
     );
 };
 
-const Item = (props) => {
+const Item = ({ item }) => {
     return (
-        <li key={props.item.objectID}
-            style={{ display: 'flex', flexDirection: 'row', justifyContent: 'start', gap: '5px' }}>
-            <span>{props.item.title}</span>
-            <span>{props.item.author}</span>
-            <span>{props.item.num_comments}</span>
-            <span>{props.item.points}</span>
+        <li style={{ display: 'flex', flexDirection: 'row', justifyContent: 'start', gap: '5px' }}>
+            <span>{item.title}</span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
         </li>
     );
     
